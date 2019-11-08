be4971eea734358a353fd49de8bd165afa35283f
minoo farsiabi <minoof@minoos-MacBook-Pro.local>
2019-11-08 13:06:45 +0330

    feat: added some getters to db class

--- src/db/index.js
+++ src/db/index.js
@@ -1,25 +1,31 @@
 import Dexie from "dexie";
-import schema from "./schema";
+import data from "./db.json";
 
 class Store {
-	constructor() {
+    constructor() {
         this.db = new Dexie("bookStore");
         this.db.version(1).stores({
-            Author: '++id,name,surename,created_at,updated_at',
-            Book: '++id,title,description,created_at,updated_at,published_at,publisher,stars,isRead,ISBN10'
+            author: '++id,name,surename,created_at,updated_at',
+            book: '++id,title,description,created_at,updated_at,published_at,publisher,stars,isRead,ISBN10'
         });
-	}
 
-    get getAuthors() {
-        return this.db.Author;
+        this.insertMany();
     }
 
-	get getBooks() {
-        return this.db.Book;
+    async insertMany() {
+        await this.db.author.bulkPut(data.authors);
+        await this.db.book.bulkPut(data.books);
+    }
+
+    async getAuthors() {
+        return await this.db.author.toArray();
+    }
+
+    async getBooks() {
+        return await this.db.book.toArray();
     }
 }
 
 const StoreDB = new Store();
-console.log(StoreDB.getAuthors)
 
 export default StoreDB;
