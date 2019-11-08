c4bb26c95c9c10fd2e8cc8313f661e40effa5fad
akram najafi <a.najafi1989@gmail.com>
2019-11-08 15:00:20 +0330

    chore: change route

--- src/screens/Dashboard/screens/Authors/index.js
+++ src/screens/Dashboard/screens/Authors/index.js
@@ -3,6 +3,7 @@ import FormGroup from "./../../../../components/FormGroup/";
 import Button from "./../../../../components/Button/";
 import Table from "./../../../../components/Table/Table";
 import data from "./../../../../db/db.json";
+import Modal from "../../../../components/Modal";
 
 function Authors() {
 	return (
@@ -11,6 +12,7 @@ function Authors() {
 				<FormGroup name="Name" />
 				<FormGroup name="surename" />
 				<Button />
+				<Modal />
 			</form>
 			<Table data={data.authors} />
 		</div>
