acd3164820e081a2882d023880098f9788ce3cca
akram najafi <a.najafi1989@gmail.com>
2019-11-08 11:23:01 +0330

    feat: add dashboard layout

---
+++ src/screens/Dashboard/components/Content/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Content() {
+  return (
+   <div>content</div>
+  );
+}
+export default Content;
---
+++ src/screens/Dashboard/components/SideMenu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SideMenu() {
+  return (
+    <nav class="nav flex-column">
+    {/* <Link class="nav-link active" to="/">Active</Link> */}
+  sidemenu
+  </nav>
+  );
+}
+export default SideMenu;
