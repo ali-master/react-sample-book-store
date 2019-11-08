27708a3a459f00ddef013de20a0a792f66eed8a9
akram najafi <a.najafi1989@gmail.com>
2019-11-08 14:32:02 +0330

    fix: merge conflicts

--- src/screens/Dashboard/components/Content/index.js
+++ src/screens/Dashboard/components/Content/index.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Switch } from "react-router-dom";
-import Books from '../../screens/Books/index.js';
-
-function Content() {
-  return (
-    <div>
-      <Switch>
-        <Route path="/dashboard/books" component={Books} />
-      </Switch>
-    </div>
-  );
-=======
 import React from "react";
 import Authors from "../../screens/Authors";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import cs from "classnames";
+import Books from "../../screens/Books/index.js";
+
 // Styles
 import styles from "./index.module.scss";
 
@@ -23,10 +11,12 @@ function Content() {
 	return (
 		<div className={cs(styles["content"], "container")}>
 			<div className="row justify-content-center">
-				<Route path="/dashboard/authors" component={Authors} />
+				<Switch>
+					<Route path="/dashboard/authors" component={Authors} />
+					<Route path="/dashboard/books" component={Books} />
+				</Switch>
 			</div>
 		</div>
 	);
->>>>>>> b9a781262f39856b0b56c527d2ea5e08dbe0ac8f
 }
 export default Content;
