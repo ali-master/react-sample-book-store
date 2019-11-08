3681b7660d2f62df6b4630b6e76d1f465889b789
akram najafi <a.najafi1989@gmail.com>
2019-11-08 11:45:44 +0330

    feat: add side menu

--- src/screens/Dashboard/components/SideMenu/index.js
+++ src/screens/Dashboard/components/SideMenu/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
 function SideMenu() {
-  return (
-    <nav class="nav flex-column">
-    {/* <Link class="nav-link active" to="/">Active</Link> */}
-  sidemenu
-  </nav>
-  );
+	return (
+		<nav class="nav flex-column">
+			<BrowserRouter>
+				<Link class="nav-link active" to="/dashboard/authors">
+					Authors
+				</Link>
+				<Link class="nav-link active" to="/dashboard/books">
+					Books
+				</Link>
+				<Link class="nav-link active" to="/dashboard/reads">
+					read books
+				</Link>
+				<Link class="nav-link active" to="/dashboard/unreads">
+					unread books
+				</Link>
+			</BrowserRouter>
+		</nav>
+	);
 }
 export default SideMenu;
